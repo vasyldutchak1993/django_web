6c70f8f5c7227a07393af6ab01bbc8ce3a246243
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-11 11:26:58 +0200

    register contact path

--- django_app/members/urls.py
+++ django_app/members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, contact
 
 app_name = 'members'
 
 urlpatterns = [
     path('', index,name='index'),
     path('about/', about,name='about'),
+    path('contact/', contact,name='contact'),
 ]
