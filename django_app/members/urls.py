35a043daf0eff96632b2ff9201657bf34138d25a
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-11 11:57:54 +0200

    register urls for members

--- django_app/members/urls.py
+++ django_app/members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, about, contact
+from .views import index, about, contact, members, member_detail
 
 app_name = 'members'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', index,name='index'),
     path('about/', about,name='about'),
     path('contact/', contact,name='contact'),
+    path('members/', members,name='members'),
+    path('members/<int:id>', member_detail,name='detail'),
 ]
