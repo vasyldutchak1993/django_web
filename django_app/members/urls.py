4ad64350bbc325bdeaddba9065abaf3541f1240a
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-11 11:22:29 +0200

    register about path

--- django_app/members/urls.py
+++ django_app/members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, about
 
 app_name = 'members'
 
 urlpatterns = [
     path('', index,name='index'),
+    path('about/', about,name='about'),
 ]
