048aa661ac7f7b6a6fe352b2db527bd544221ed6
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-11 11:43:08 +0200

    create and register member admin

--- django_app/members/admin.py
+++ django_app/members/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Member
+
+
 # Register your models here.
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'email')
+    search_fields = ('firstname', 'lastname', 'email')
+    list_filter = ('firstname', 'lastname', 'email')
+    ordering = ('firstname', 'lastname')
