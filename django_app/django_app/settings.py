13ffa9461eb73d72bdd3d2312f81a8bea9729b3f
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-14 19:48:24 +0200

    add config to .env make .env_example

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -12,14 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+load_dotenv(BASE_DIR.parent / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0tz88xf1#&tc-%zhrwtm#ib$1f8ccr&tm9kh!lug_!yq-0la%w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -112,7 +113,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'django_app','members', 'static'),
+    os.path.join(BASE_DIR,'members', 'static'),
 ]
 
 # Default primary key field type
