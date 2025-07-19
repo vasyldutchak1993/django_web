52b61fa3eeddccb536de0d09261abee0669574a7
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-19 14:26:25 +0200

    finished task 4

--- django_app/members/urls.py
+++ django_app/members/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import index, about, contact, members, member_detail, process_get_form_request, category_selection_form, submit_feedback
 
 app_name = 'members'
@@ -12,4 +14,5 @@ urlpatterns = [
     path('process_get_form/',process_get_form_request,name='process_get_form'),
     path('category_selection_form/',category_selection_form,name='category_selection_form'),
     path('submit_feedback/',submit_feedback,name='submit_feedback'),
+    path('static_template_view/',TemplateView.as_view(template_name='task_class_view/static_template_view.html'),name='static_template_view'),
 ]
