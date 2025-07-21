2700f84d5b64e129ae3cca78e189c0ea24fc578c
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-07-21 15:13:50 +0200

    install guicorn change env path

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-load_dotenv(BASE_DIR.parent / '.env')
+load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
